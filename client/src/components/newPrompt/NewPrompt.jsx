07cc6bb21d3f697edfe560080a52de986fde0db1
zordhalo <krawczakluke@gmail.com>
2024-11-20 13:28:26 -0500

    refracture for backend

---
+++ client/src/components/newPrompt/NewPrompt.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useRef } from 'react'
+import './newPrompt.css'
+
+const NewPrompt = () => {
+
+  const endRef = useRef(null);
+
+  useEffect(() => {
+    endRef.current.scrollIntoView({ behavior: "smooth" })
+  }, [])
+
+  return (
+    <>
+    {/* Add a new chat */} 
+      <div className="endChat" ref={endRef}></div>
+      <form className="newForm">
+        <label htmlFor="file">
+          <img src="/attachment.png" alt="" />
+        </label>
+        <input id="file" type="file" multiple={false} hidden />
+        <input type="text" placeholder="Type a message"/>
+        <button>
+          <img src="/arrow.png" alt="" />
+        </button>
+      </form>
+    </>
+  )
+}
+
+export default NewPrompt
