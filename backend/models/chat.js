10de23c7911b968bd2644b4d57942b4dc8214575
zordhalo <krawczakluke@gmail.com>
2024-11-21 17:01:03 -0500

    Revert "models updated"
    This reverts commit deb740d68edf192f58dba736edf215c7a5aa86de.

--- backend/models/chat.js
+++ backend/models/chat.js
@@ -1,34 +1,31 @@
 import mongoose from "mongoose";
 
-const chatSchema = new mongoose.Schema(
-  {
+const chatSchema = new mongoose.Schema({
     userId: {
-      type: String,
-      required: true,
+    type: String,
+    required: true,
     },
-    history: [
-      {
-        role: {
-          type: String,
-          enum: ["user", "model"],
-          required: true,
-        },
-        parts: [
-          {
-            text: {
-              type: String,
-              required: true,
+    history:[
+        {
+            role:{
+                type: String,
+                enum: ["user", "model"],
+                required: true,
             },
-          },
-        ],
-        img: {
-          type: String,
-          required: false,
-        },
-      },
-    ],
-  },
-  { timestamps: true }
-);
+            parts:[
+                {
+                    text:{
+                    type: String,
+                    required: true,
+                    },
+                },
+            ],
+            img:{
+                type: String,
+                required: false,
+            },
+        }
+    ]
+},{timestamps: true});
 
 export default mongoose.models.chat || mongoose.model("chat", chatSchema);
--- backend/models/userChats.js
+++ backend/models/userChats.js
@@ -1,30 +1,27 @@
 import mongoose from "mongoose";
 
-const userChatsSchema = new mongoose.Schema(
-  {
+const userChatsSchema = new mongoose.Schema({
     userId: {
-      type: String,
-      required: true,
+        type: String,
+        required: true,
     },
     chats: [
-      {
-        _id: {
-          type: String,
-          required: true,
-        },
-        title: {
-          type: String,
-          required: true,
-        },
-        createdAt: {
-          type: Date,
-          default:Date.now()
-        },
-      },
-    ],
-  },
-  { timestamps: true }
-);
+        {
+            chatId: {
+                type: mongoose.Schema.Types.ObjectId,
+                required: true,
+            },
+            chatTitle: {
+                type: String,
+                required: true,
+            },
+            createdAt: {
+                type: Date,
+                default: Date.now,
+            },
+        }
+    ]
+},
+{ timestamps: true });
 
-export default mongoose.models.userchats ||
-  mongoose.model("userchats", userChatsSchema);
+export default mongoose.models.userchats || mongoose.model("userchat", userChatsSchema);
